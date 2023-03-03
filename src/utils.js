e240d130c98101063f4db5aed88e67fb0ca605aa
pedro <antonioaren2@gmail.com>
2023-03-03 02:09:35 +0100

    Feat: data write in a json as a result

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,18 @@ function getCouponsData() {
     return coupons_object.coupons
 }
 
+// write json data in a file call result.json
+function writeJsonData(jsonData) {
+    //Check if is type json
+    if (typeof jsonData === 'object') {
+        throw new Error('The data is not a json object')
+    }
+
+    fs.writeFileSync('assets/result.json', jsonData)
+}
+
 //Export this function to be used in other files
 module.exports = {
-    getCouponsData
+    getCouponsData,
+    writeJsonData
 }
