0180afb7c1c6869276dd381e0948d0db04b8f6a5
antonioaren@hotmail.com <pedro.arenas@with-madrid.com>
2023-03-07 09:39:24 +0100

    feat: jest added and a little utils test

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,23 @@
 const fs = require('fs')
 
-function getCouponsData() {
-    const coupons_json = fs.readFileSync('assets/coupons.json', 'utf8')
-    const coupons_object = JSON.parse(coupons_json)
-    return coupons_object.coupons
+function getCouponsData(path = 'assets/coupons.json') {
+	const coupons_json = fs.readFileSync(path, 'utf8');
+	const coupons_object = JSON.parse(coupons_json);
+	return coupons_object.coupons;
 }
 
 // write json data in a file call result.json
-function writeJsonData(jsonData) {
-    //Check if is type json
-    if (typeof jsonData === 'object') {
-        throw new Error('The data is not a json object')
-    }
+function writeJsonData(jsonData, path = 'assets/result.json') {
+	//Check if is type json
+	if (typeof jsonData === 'object') {
+		throw new Error('The data is not a json object');
+	}
 
-    fs.writeFileSync('assets/result.json', jsonData)
+	fs.writeFileSync(path, jsonData);
 }
 
 //Export this function to be used in other files
 module.exports = {
-    getCouponsData,
-    writeJsonData
-}
+	getCouponsData,
+	writeJsonData,
+};
